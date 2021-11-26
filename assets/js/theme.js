fa69a919867f5968bcbc1c773f84d3afdacc6ce1
Ryan Sam <rsamaeian@protonmail.com>
2021-11-26 08:18:06 +0330

    Home Page

---
+++ assets/js/theme.js
@@ -0,0 +1,48 @@
+// ┌┬┐┬ ┬┌─┐┌┬┐┌─┐
+//  │ ├─┤├┤ │││├┤
+//  ┴ ┴ ┴└─┘┴ ┴└─┘
+
+// Store the theme
+let darkTheme = localStorage.getItem('darkTheme');
+const themeToggle = document.querySelector('#themeButton');
+const bodyBackground = document.getElementById('#body');
+
+// Apply Dark theme
+const enableDark = () => {
+  document.body.classList.add('darktheme');
+  localStorage.setItem('darkTheme', 'enabled');
+  themeToggle.innerHTML = `<i id="themeButton__icon" icon-name="sun"></i>`;
+  lucide.createIcons();
+};
+
+// Remove Dark theme
+const disableDark = () => {
+  document.body.classList.remove('darktheme');
+  localStorage.setItem('darkTheme', null);
+  themeToggle.innerHTML = `<i id="themeButton__icon" icon-name="moon"></i>`;
+  lucide.createIcons();
+};
+
+//Toggle theme
+if (darkTheme === 'enabled') {
+  // Temporarily disable transitions when changing theme on startup
+  document.body.classList.add('notransition');
+  enableDark();
+  document.body.offsetHeight; // Trigger reflow to flush CSS changes
+  document.body.classList.remove('notransition');
+} else {
+  enableDark();
+}
+
+themeToggle.addEventListener('click', () => {
+  darkTheme = localStorage.getItem('darkTheme');
+  if (darkTheme !== 'enabled') {
+    enableDark();
+  } else {
+    enableDark();
+  }
+});
+
+if (CONFIG.imageBackground) {
+  document.body.classList.add('withImageBackground');
+}
